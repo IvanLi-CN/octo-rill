9250ba055a9850d969c961cf9395ec4a72ed2682
Ivan Li <ivanli2048@gmail.com>
2026-07-09 04:11:10 +0800

    fix(web): defer app import until demo bootstrap
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,34 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { AppToastProvider } from "@/components/feedback/AppToast";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { prepareDemoRuntime, shouldPrepareDemoRuntime } from "@/demo/runtime";
-import { AppQueryProvider } from "@/query/queryClient";
-import { ThemeProvider } from "@/theme/ThemeProvider";
-import { VersionMonitorProvider } from "@/version/versionMonitor";
 import "./index.css";
-import App from "./App.tsx";
 
-function renderApp() {
-	createRoot(document.getElementById("root")!).render(
-		<StrictMode>
-			<AppQueryProvider>
-				<ThemeProvider>
-					<TooltipProvider>
-						<AppToastProvider>
-							<VersionMonitorProvider>
-								<App />
-							</VersionMonitorProvider>
-						</AppToastProvider>
-					</TooltipProvider>
-				</ThemeProvider>
-			</AppQueryProvider>
-		</StrictMode>,
-	);
+async function renderApp() {
+	const { renderApp } = await import("./renderApp");
+	renderApp(document.getElementById("root")!);
 }
 
 if (shouldPrepareDemoRuntime()) {
 	void prepareDemoRuntime().then(renderApp);
 } else {
-	renderApp();
+	void renderApp();
 }
